80cd18acdbbfde65c8053b954c242d6bf5ca5b3a
chaseheim <95732103+chaseheim@users.noreply.github.com>
2022-05-07 22:39:41 -0500

    Add Chinese localization

--- application/localization/babelutils.py
+++ application/localization/babelutils.py
@@ -5,7 +5,11 @@ from flask import session
 def get_locale():
     """Return the desired langurage from the session setting"""
 
-    if session.get('localization_preference') == 'es':
-        return 'es'
-    else:
-        return 'en'
+    match session.get('localization_preference'):
+        case 'es':
+            return 'es'
+        case 'zh':
+            return 'zh'
+    
+    # If all else fails, return English
+    return 'en'
