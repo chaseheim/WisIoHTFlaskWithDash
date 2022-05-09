4053b9df17e4b862098f3d9179c9c7a1c5d410cc
chaseheim <95732103+chaseheim@users.noreply.github.com>
2022-05-09 14:33:25 -0500

    Fix Elastic Beanstalk get_locale() bug

--- application/localization/babelutils.py
+++ application/localization/babelutils.py
@@ -4,12 +4,8 @@ from flask import session
 @babel.localeselector
 def get_locale():
     """Return the desired langurage from the session setting"""
-
-    match session.get('localization_preference'):
-        case 'es':
-            return 'es'
-        case 'zh':
-            return 'zh'
     
-    # If all else fails, return English
-    return 'en'
+    if session.get('localization_preference') != None:
+        return session.get('localization_preference')
+
+    return None
